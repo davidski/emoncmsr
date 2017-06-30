15da4222fda8a2b569eff2808c4dc5e53613161a
David F. Severski <davidski@deadheaven.com>
2017-06-29 17:30:00 -0700

    Make utility functions a bit more robust per suggestions from httr vignette

--- R/util.R
+++ R/util.R
@@ -7,23 +7,35 @@
 #' @param post_body For POST calls, any fields to specify in the POST body.
 #' @param verbose If set (boolean), will warn on the query parameters to be sent
 #' @export
-send_emon_request <- function(uri, params = NULL, method = "GET", post_body = NULL, verbose = FALSE) {
-  url <- paste0(emoncms_uri(), uri)
+send_emon_request <- function(uri, params = NULL, method = "GET",
+                              post_body = NULL, verbose = FALSE) {
+  url <- httr::modify_url(emoncms_uri(), path = uri)
+  ua <- user_agent("http://github.com/davidski/emoncmsr")
+
   # currently add the API key to the query string as header support is
   # broken on emoncms.org and only available on emoncms master as of 6/27/17
   query_params <- list(apikey = emoncms_api_key())
   query_params <- c(query_params, params)
+
   if (verbose)
     warning("Sending query params", (params))
+
+  # send either a POST or GET request, per passed parameters
   if (method != "POST") {
-    response <- httr::GET(url, query = query_params,
+    response <- httr::GET(url, query = query_params, ua,
                         httr::add_headers(Authorization =
                                              paste("Bearer", emoncms_api_key())))
   } else {
-    response <- httr::POST(url, query = query_params, body = post_body,
+    response <- httr::POST(url, query = query_params, body = post_body, ua,
                           httr::add_headers(Authorization =
                                                paste("Bearer", emoncms_api_key())))
   }
+
+  # throw an error if we get an HTTP error back from the API
+  if httr::http_error(response) {
+    stop("Error from emoncms API.")
+  }
+
   response
 }
 
@@ -32,7 +44,14 @@ send_emon_request <- function(uri, params = NULL, method = "GET", post_body = NU
 #' @return The URI to the emonCMS endpoint.
 #' @export
 emoncms_uri <- function() {
-  Sys.getenv("EMONCMS_URI")
+  uri <- Sys.getenv("EMONCMS_URI")
+
+  if (identical(uri, "")) {
+    stop("Please set env var EMONCMS_URI to the base of your emoncms API endpoint",
+         call. = FALSE)
+  }
+
+  uri
 }
 
 #' Fetch the emonCMS API key from the environment.
@@ -40,5 +59,13 @@ emoncms_uri <- function() {
 #' @return The API key to use for the emonCMS server.
 #' @export
 emoncms_api_key <- function() {
-  Sys.getenv("EMONCMS_API_KEY")
+  api_key <- Sys.getenv("EMONCMS_API_KEY")
+
+  if (identical(api_key, "")) {
+    stop("Please set env var EMONCMS_API_KEY to your emoncms API token",
+         call. = FALSE)
+  }
+
+  api_key
+
 }
